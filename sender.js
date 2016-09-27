aaaba9356308df1e418757142d20ef4d7a31d2fc
Mew <a0087510@nus.edu.sg>
2016-09-27 11:29:32 +0800

    update application id

--- sender.js
+++ sender.js
@@ -15,7 +15,7 @@
 /**
  * global variables
  */
-var applicationID = 'YOUR_APP_ID';
+var applicationID = '0D43DACE';
 var MESSAGE_NAMESPACE = 'urn:x-cast:com.google.cast.sample.mediaplayer';
 var currentMedia = null;
 var currentVolume = 0.5;
